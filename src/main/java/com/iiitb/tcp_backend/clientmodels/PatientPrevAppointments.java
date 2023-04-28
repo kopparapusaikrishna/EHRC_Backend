9e499b6302eb67e05ced79e733b2023948183951
Saikrishna <kopparapusaikrishna14@gmail.com>
2023-04-28 23:13:05 +0530

    'Modified patient-previous-appointments API'

--- src/main/java/com/iiitb/tcp_backend/clientmodels/PatientPrevAppointments.java
+++ src/main/java/com/iiitb/tcp_backend/clientmodels/PatientPrevAppointments.java
@@ -3,24 +3,24 @@ package com.iiitb.tcp_backend.clientmodels;
 import java.sql.Date;
 
 public class PatientPrevAppointments {
-    private Date appointmemt_date;
+    private Date appointment_date;
     private String doctor_dept;
     private String doctor_name;
     private int appointment_id;
 
     public PatientPrevAppointments(Date appointmemt_date, String doctor_dept, String doctor_name, int appointment_id) {
-        this.appointmemt_date = appointmemt_date;
+        this.appointment_date = appointmemt_date;
         this.doctor_dept = doctor_dept;
         this.doctor_name = doctor_name;
         this.appointment_id = appointment_id;
     }
 
     public Date getAppointmemt_date() {
-        return appointmemt_date;
+        return appointment_date;
     }
 
     public void setAppointmemt_date(Date appointmemt_date) {
-        this.appointmemt_date = appointmemt_date;
+        this.appointment_date = appointmemt_date;
     }
 
     public String getDoctor_dept() {
